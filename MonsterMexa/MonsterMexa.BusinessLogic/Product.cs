a52901b7576d5691d647e0e6e46bb52ebb420805
Hib <h1berd@mail.ru>
2022-07-03 09:45:37 +0300

    feat: draft crud products

---
+++ MonsterMexa/MonsterMexa.BusinessLogic/Product.cs
@@ -0,0 +1,9 @@
+﻿namespace MonsterMexa.API
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public int Size { get; set; }
+    }
+}
