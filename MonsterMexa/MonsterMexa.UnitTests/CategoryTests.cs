b94886d0cb3f655e26372b386c6d1ec9e402d6d0
Hib <h1berd@mail.ru>
2022-07-23 16:06:12 +0300

    add unit-tests

---
+++ MonsterMexa/MonsterMexa.UnitTests/CategoryTests.cs
@@ -0,0 +1,35 @@
+﻿using MonsterMexa.Domain;
+
+namespace MonsterMexa.UnitTests
+{
+    public class CategoryTests
+    {
+        [Fact]
+        public void Create_IsValid_ShouldReturnCategory()
+        {
+            // arrange
+            string name = "Test";
+
+            // act
+            var category = Category.Create(name);
+
+            // assert
+            Assert.True(category.IsSuccess);
+            Assert.Equal("Test", category.Value.Name);
+        }
+
+        [Fact]
+        public void Create_IsNotValid_ShouldReturnCategory()
+        {
+            // arrange
+            string name = string.Empty;
+
+            // act
+            var category = Category.Create(name);
+
+            // assert
+            Assert.True(category.IsFailure);
+            Assert.Equal("Name cannot be null or whitespace", category.Error);
+        }
+    }
+}
